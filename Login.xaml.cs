e985a0555d6e30ad90e04efb02454b31d49969ed
BotDoc <trfdoc@gmail.com>
2018-05-29 13:21:33 +0300

    Basic sql login operations

--- Login.xaml.cs
+++ Login.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,5 +25,66 @@ namespace Learn_English
         {
             InitializeComponent();
         }
+
+        private void btnSubmit_Click(object sender, RoutedEventArgs e)
+        {
+            SqlConnection sqlCon = new SqlConnection(@"Data Source=DAIENKAIENTEI;Initial Catalog=Learn_English;Integrated Security=True");
+            try
+            {
+                if (sqlCon.State == ConnectionState.Closed)
+                    sqlCon.Open();
+                String query = "SELECT COUNT(1) FROM Users WHERE username=@Username AND password=@Password";
+                SqlCommand sqlCmd = new SqlCommand(query, sqlCon);
+                sqlCmd.CommandType = CommandType.Text;
+                sqlCmd.Parameters.AddWithValue("@Username", txtUsername.Text);
+                sqlCmd.Parameters.AddWithValue("@Password", txtPassword.Password);                
+                int count = Convert.ToInt32(sqlCmd.ExecuteScalar());
+                if (count == 1)
+                {
+                    String rolequery = "SELECT usertype FROM Users WHERE username=@Username AND password=@Password";
+                    SqlCommand rolesqlCmd = new SqlCommand(rolequery, sqlCon);
+                    rolesqlCmd.CommandType = CommandType.Text;
+                    rolesqlCmd.Parameters.AddWithValue("@Username", txtUsername.Text);
+                    rolesqlCmd.Parameters.AddWithValue("@Password", txtPassword.Password);
+                    SqlDataReader reader = rolesqlCmd.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        var roleString = reader.GetString(0);
+                        if (roleString == "student")
+                        {
+                            StudentMain studentMain = new StudentMain();
+                            studentMain.Show();
+                            this.Close();
+                            break;
+                        }
+                        if (roleString == "teacher")
+                        {
+                            TeacherMain teacherMain = new TeacherMain();
+                            teacherMain.Show();
+                            this.Close();
+                            break;
+
+                        }
+                        else
+                        {
+                            MessageBox.Show("Error in database. Please contact the admin.");
+                        }
+                    }                      
+                }
+                else
+                {
+                    MessageBox.Show("Username or password is incorrect. Please contact the admin.");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                sqlCon.Close();
+            }
+        }
+
     }
 }
